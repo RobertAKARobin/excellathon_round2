cdd68f68d28fe1ad01f8c8583fffc8b3c1308461
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-30 14:45:03 -0500

    Damn Javascript

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 try{
   var env = require("./env.json");
 }catch(e){
@@ -17,27 +20,26 @@ var stations;
   });
 }());
 
-app.use(bodyParser.json());
-
 app.get("/", function(req, res){
   res.json({success: true});
 });
 
 app.post("/", function(req, res){
+  var body = req.body;
   var params = {
-    Lat: (req.body.latitude || null),
-    Lon: (req.body.longitude || null),
-    Radius: (req.body.radius || null)
+    Lat: (body.latitude || null),
+    Lon: (body.longitude || null),
+    Radius: (body.radius || null)
   }
   api("jStationEntrances", params, getStationEntrances);
 
   function getStationEntrances(data){
     api("jStationInfo", {StationCode: data.Entrances[0]["StationCode1"]}, function(dat){
-      res.json({
+      res.send({
         station: dat.Name,
-        stationLat: dat.Lat.toFixed(6),
-        stationLong: dat.Lon.toFixed(6)
-      })
+        stationLat: dat.Lat,
+        stationLong: Number(dat.Lon.toFixed(6))
+      });
     });
   }
 });
